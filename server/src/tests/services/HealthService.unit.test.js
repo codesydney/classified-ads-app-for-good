64cbf61753ad922d8998b9b46166955cdb0bab46
Promie Yutasane <promie.yutasane@gmail.com>
2024-02-17 13:59:51 +1100

    CSD-12 fixed wording

--- server/src/tests/services/HealthService.unit.test.js
+++ server/src/tests/services/HealthService.unit.test.js
@@ -1,6 +1,6 @@
 const { addTwoNumbers } = require('../../services/HealthService')
 
-describe('Health Service', () => {
+describe('HealthService', () => {
   describe('#addTwoNumbers', () => {
     it('should add two numbers with the correct output', () => {
       expect(addTwoNumbers(1, 2)).toBe(3)
