f1580cda88e212e17d6576be2ee4fdd426e2adb3
Promie Yutasane <promie.yutasane@gmail.com>
2024-02-17 12:56:45 +1100

    CSD-12 integration tests for the health endpoint

--- server/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>'],
-  testMatch: ['**/?(*.)+(test).+(ts)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js'],
-  testEnvironment: 'node',
-}
