3e4bd4b6c73462e6acd84cf7054e913262c91cfd
Promie Yutasane <promie.yutasane@gmail.com>
2024-02-21 06:25:35 +1100

    CA-16 address the prettier check

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const mongoURI = process.env.NODE_ENV === 'test' ?
-  (process.env.TEST_MONGO_URI || 'mongodb://localhost:27017/integration') :
-  (process.env.MONGO_URI || 'mongodb://localhost:27017/caafg');
+const mongoURI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGO_URI || 'mongodb://localhost:27017/integration'
+    : process.env.MONGO_URI || 'mongodb://localhost:27017/caafg'
 
 const Mongoose = () => {
   const initialiseMongoConnection = () => {
     return new Promise((resolve, reject) => {
-      mongoose.connect(mongoURI)
+      mongoose
+        .connect(mongoURI)
         .then(() => console.log('Connected to MongoDB...'))
         .then(() => resolve(mongoose))
-        .catch((err) => {
-          console.error('Connection error:', err);
-          reject(err);
-        });
+        .catch(err => {
+          console.error('Connection error:', err)
+          reject(err)
+        })
 
-      const db = mongoose.connection;
+      const db = mongoose.connection
 
-      db.on('error', (err) => {
-        console.error('Connection error:', err);
-        reject(err);
-      });
-    });
-  };
+      db.on('error', err => {
+        console.error('Connection error:', err)
+        reject(err)
+      })
+    })
+  }
 
-  return { initialiseMongoConnection };
-};
+  return { initialiseMongoConnection }
+}
 
-module.exports = Mongoose;
+module.exports = Mongoose
