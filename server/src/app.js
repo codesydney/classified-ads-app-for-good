2eb1495c8d3c04ac33887b4d12cd0324473bd202
Promie Yutasane <promie.yutasane@gmail.com>
2024-02-22 19:45:17 +1100

    CA-23 setup swagger to run on local server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerSpecs = require('./swaggerConfig')
 const HealthRoute = require('./routes/HealthRoute')
 
 const app = express()
@@ -8,5 +10,13 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/v1/health', HealthRoute)
+
+app.use('/', swaggerUi.serve, async (req, res) => {
+  if (req.path === '/') {
+    return swaggerUi.setup(swaggerSpecs)(req, res)
+  } else {
+    return res.next()
+  }
+})
 
 module.exports = app
