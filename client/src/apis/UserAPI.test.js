57897a0a482a32c5a9f8dfde4eddc714c3450b5a
Nathan Sou <nathanksou@gmail.com>
2024-02-23 21:50:21 +1100

    prettier fix

--- client/src/apis/UserAPI.test.js
+++ client/src/apis/UserAPI.test.js
@@ -3,15 +3,19 @@ import { UserAPI } from './UserAPI'
 
 describe('UserAPI', () => {
   test('should return response if UserAPI.getAll is successful', async () => {
-    const getAllResponse = { data: {users: [{name: 'John Doe'}]}}
+    const getAllResponse = { data: { users: [{ name: 'John Doe' }] } }
     const spy = jest.spyOn(api, 'get').mockReturnValue(getAllResponse)
     expect(await UserAPI.getAll()).toEqual([{ name: 'John Doe' }])
     expect(spy).toHaveBeenCalledWith('/users', { signal: undefined })
   })
+
   test('should return response if UserAPI.search is successful', async () => {
-    const search = { data: {users: [{name: 'John Doe'}]}}
+    const search = { data: { users: [{ name: 'John Doe' }] } }
     const spy = jest.spyOn(api, 'get').mockReturnValue(search)
     expect(await UserAPI.search('lawyer')).toEqual([{ name: 'John Doe' }])
-    expect(spy).toHaveBeenCalledWith('/users/search', { params: { q: 'lawyer' }, signal: undefined })
+    expect(spy).toHaveBeenCalledWith('/users/search', {
+      params: { q: 'lawyer' },
+      signal: undefined,
+    })
   })
 })
