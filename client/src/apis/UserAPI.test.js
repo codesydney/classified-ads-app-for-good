3f9fecc5a1bd5fc1f71d09182535dbb2124b449a
Nathan Sou <nathanksou@gmail.com>
2024-02-23 21:26:19 +1100

    update UserAPI tests

--- client/src/apis/UserAPI.test.js
+++ client/src/apis/UserAPI.test.js
@@ -1,5 +1,17 @@
+import { api } from './configs/axiosConfig'
 import { UserAPI } from './UserAPI'
 
 describe('UserAPI', () => {
-  test('', () => {})
+  test('should return response if UserAPI.getAll is successful', async () => {
+    const getAllResponse = { data: {users: [{name: 'John Doe'}]}}
+    const spy = jest.spyOn(api, 'get').mockReturnValue(getAllResponse)
+    expect(await UserAPI.getAll()).toEqual([{ name: 'John Doe' }])
+    expect(spy).toHaveBeenCalledWith('/users', { signal: undefined })
+  })
+  test('should return response if UserAPI.search is successful', async () => {
+    const search = { data: {users: [{name: 'John Doe'}]}}
+    const spy = jest.spyOn(api, 'get').mockReturnValue(search)
+    expect(await UserAPI.search('lawyer')).toEqual([{ name: 'John Doe' }])
+    expect(spy).toHaveBeenCalledWith('/users/search', { params: { q: 'lawyer' }, signal: undefined })
+  })
 })
