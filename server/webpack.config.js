74f2dbb02735df5ab4bc478832a7ec9440efefe1
Promie Yutasane <promie.yutasane@gmail.com>
2024-02-25 17:06:10 +1100

    CA-26 updated webpack and start:prodserver script

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,43 +1,20 @@
 const path = require('path')
-const webpack = require('webpack')
-const TerserPlugin = require('terser-webpack-plugin')
 const { IgnorePlugin } = require('webpack')
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'bundle.js',
   },
   target: 'node',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
   plugins: [
     new IgnorePlugin({
       resourceRegExp:
         /^(kerberos|@mongodb-js\/zstd|@aws-sdk\/credential-providers|gcp-metadata|snappy|socks|aws4|mongodb-client-encryption)$/,
     }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new NodePolyfillPlugin(),
   ],
   ignoreWarnings: [
     /Critical dependency: the request of a dependency is an expression/,
