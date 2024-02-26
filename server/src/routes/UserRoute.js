a6c6b3c130897ed05223c2d33f0d18de4124126d
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-02-26 15:38:42 +1100

    move routes from userRoute to UserRoute file

--- server/src/routes/UserRoute.js
+++ server/src/routes/UserRoute.js
@@ -5,4 +5,12 @@ const router = express.Router()
 
 router.get('/', UserController.getUsers)
 
+router.post('/signup', UserController.signup)
+
+router.post('/signin', UserController.signin)
+
+router.post('/request-reset-password', UserController.requestResetPassword)
+
+router.post('/reset-password', UserController.resetPassword)
+
 module.exports = router
