dbbee74991974d6a408bc94259becf17857f357f
Promie Yutasane <promie.yutasane@gmail.com>
2024-02-28 14:20:40 +1100

    fix build updating webpack.config.js

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,22 +1,37 @@
 const path = require('path')
-const { IgnorePlugin } = require('webpack')
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   target: 'node',
+  externals: [/node_modules/],
   plugins: [
-    new IgnorePlugin({
-      resourceRegExp:
-        /^(kerberos|@mongodb-js\/zstd|@aws-sdk\/credential-providers|gcp-metadata|snappy|socks|aws4|mongodb-client-encryption)$/,
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^(node-gyp|npm|bson-ext|snappy\/package.json)$/,
     }),
   ],
-  ignoreWarnings: [
-    /Critical dependency: the request of a dependency is an expression/,
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'null-loader',
+      },
+    ],
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+      path: false,
+      os: false,
+      'mock-aws-s3': false,
+      'aws-sdk': false,
+      nock: false,
+    },
+  },
+  ignoreWarnings: [warning => /Can't resolve/.test(warning.message)],
 }
