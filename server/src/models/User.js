e3da8793f22dd0a6214099c2fa56d8d419db6f10
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-05 19:35:46 +1100

    CA-34 removed bcrypt and mongoose-bcrypt and replace with bcryptjs

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcryptjs')
 
 const emailRegex =
   /^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$/
@@ -106,5 +107,28 @@ const UserSchema = new Schema({
   },
 })
 
-UserSchema.plugin(require('mongoose-bcrypt'))
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err)
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err)
+
+      this.password = hash
+      next()
+    })
+  })
+})
+
+UserSchema.methods.verifyPassword = function (candidatePassword) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) return reject(err)
+      resolve(isMatch)
+    })
+  })
+}
+
 module.exports = mongoose.model('User', UserSchema)
--- server/src/utils/resetTokens.js
+++ server/src/utils/resetTokens.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 // Generate token to send in email
 // Hash token for db storage.
