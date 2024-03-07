2404918730fe8e8d5c739e3507d7fbc8d1fd1af7
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-08 07:58:51 +1100

    CA-37 revert back getAsync for health controller

--- server/src/controllers/HealthController.js
+++ server/src/controllers/HealthController.js
@@ -1,10 +1,8 @@
-const catchAsync = require('../utils/catchAsync')
-
-const getHealth = catchAsync((req, res, _next) => {
+const getHealth = (req, res, _next) => {
   return res.status(200).json({
     status: 'OK',
   })
-})
+}
 
 module.exports = {
   getHealth,
