6a70ccbe40a8a5c6c75164c48343ba945bb69f60
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-24 05:33:56 +1100

    CA-79 added the authenticationStatus middleware

---
+++ server/src/middleware/authenticationStatus.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const authenticationStatus = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  let token = null
+
+  if (authHeader) {
+    const parts = authHeader.split(' ')
+    if (parts.length === 2 && parts[0] === 'Bearer') {
+      token = parts[1]
+    }
+  } else if (req.query.token) {
+    token = req.query.token
+  }
+
+  if (!token) {
+    req.isAuthenticated = false
+    next()
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      const error = new Error('Unauthorized')
+      error.statusCode = 401
+      throw error
+    }
+
+    req.isAuthenticated = true
+    next()
+  })
+}
+
+module.exports = {
+  authenticationStatus,
+}
