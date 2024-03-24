f25d95f46fb4aafee11e7e735c73b6952b23d7f0
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-03-24 19:07:59 +1100

    Contact page styling complete. Illustration added.

--- client/src/components/contact/ContactForm.jsx
+++ client/src/components/contact/ContactForm.jsx
@@ -1,3 +1,5 @@
 const ContactForm = () => {
   return <form></form>
 }
+
+export default ContactForm
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,8 +1,28 @@
+import ContactForm from '../components/contact/ContactForm'
+import contactIllustration from '../assets/contactIllustration.svg'
+
 const Contact = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the contact page.</p>{' '}
+    <div className="my-10 flex-grow flex items-center">
+      <div className="px-4 max-w-{1200px] mx-auto md:grid md:grid-cols-2">
+        <div className="mb-5">
+          <h1 className="text-4xl text-bold text-primary text-center mb-5">
+            Contact Us
+          </h1>
+          <p className="text-center mb-10">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            omnis cum harum aliquid qui perspiciatis. Dolor voluptate odit
+            mollitia nostrum aliquid dignissimos officia at et.
+          </p>
+          <img
+            src={contactIllustration}
+            alt=""
+            aria-hidden="true"
+            className="md:relative z-[-1]"
+          />
+        </div>
+        <ContactForm />
+      </div>
     </div>
   )
 }
