896765382d600d6c88c133cd354a4615bd6efbe0
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-03-24 12:22:42 +1100

    Add yup validation for general information form. Only validate dirty fields. Non-required dirty fields are nullable'

--- client/src/schema/index.js
+++ client/src/schema/index.js
@@ -1,7 +1,13 @@
 import * as yup from 'yup'
+import { useFormContext } from 'react-hook-form'
 
 const emailRegex =
   /^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$/
+
+const phoneRegex =
+  /^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$/
+const postcodeRegex = /^[0-9]{4}$/
+const urlRegex = /^(https?:\/\/)[^\s/$.?#].[^\s]*$/
 
 const signUpSchema = yup
   .object({
@@ -76,9 +82,72 @@ const passwordResetRequestSchema = yup
   })
   .required()
 
+// Validation for general information form. Only validates dirty fields.
+const generalInformationSchema = yup.object({
+  firstName: yup.string().when('firstName', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.firstName,
+    then: yup
+      .string()
+      .required('First name is required')
+      .min(2, 'First name must be at least 2 character')
+      .max(50, 'First name cannot exceed 50 characters'),
+  }),
+  lastName: yup.string().when('lastName', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.lastName,
+    then: yup
+      .string()
+      .required('Last name is required')
+      .min(2, 'Last name must be at least 2 character')
+      .max(50, 'Last name cannot exceed 50 characters'),
+  }),
+  email: yup.string().when('email', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.email,
+    then: yup.string().email('Invalid email').required('Email is required'),
+  }),
+  phone: yup.string().when('phone', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.phone,
+    then: yup
+      .string()
+      .nullable()
+      .matches(phoneRegex, 'Please enter a valid 10-digit phone number'),
+  }),
+  suburb: yup.string().when('suburb', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.suburb,
+    then: yup
+      .string()
+      .nullable()
+      .min(3, 'Suburb must be at least 3 character')
+      .max(50, 'Suburb cannot exceed 50 characters'),
+  }),
+  postcode: yup.string().when('postcode', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.postcode,
+    then: yup
+      .string()
+      .nullable()
+      .matches(postcodeRegex, 'Please enter a valid Australian Postcode'),
+  }),
+  facebookName: yup.string().when('facebookName', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.facebookName,
+    then: yup
+      .string()
+      .nullable()
+      .min(3, 'Facebook name must be at least 3 character')
+      .max(50, 'Facebook name cannot exceed 50 characters'),
+  }),
+  story: yup.string().when('story', {
+    is: (value, schema) => useFormContext().formState.dirtyFields.story,
+    then: yup
+      .string()
+      .nullable()
+      .min(3, 'Facebook story name must be at least 3 character')
+      .max(500, 'Facebook story name cannot exceed 50 characters'),
+  }),
+})
+
 export {
   signUpSchema,
   loginSchema,
   resetPasswordSchema,
   passwordResetRequestSchema,
+  generalInformationSchema,
 }
