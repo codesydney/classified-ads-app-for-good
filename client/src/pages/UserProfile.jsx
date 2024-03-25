c9a2c7b19c4b3e8e3c05f10c16a9a45523f81508
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-25 20:43:04 +1100

    CA-86 user not found

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { toast } from 'react-hot-toast'
 import { useAppDispatch } from '../store.js'
 import { getUserProfile } from '../features/users/usersAction.js'
 
@@ -12,7 +13,15 @@ const UserProfile = () => {
   const { userProfile } = useSelector(state => state.users)
 
   useEffect(() => {
-    dispatch(getUserProfile({ userId }))
+    const fetchUserProfile = async () => {
+      const response = await dispatch(getUserProfile({ userId }))
+
+      if (response.error) {
+        toast.error('User Not Found')
+      }
+    }
+
+    fetchUserProfile()
   }, [userId])
 
   return (
