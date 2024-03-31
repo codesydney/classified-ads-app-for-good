a7cd66b7bb170e5b4b25f92526b238a169ddd0b1
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-31 16:09:52 +1100

    CA-92 updated the UserImg component to use fullname and alumniProfilePicture instead

--- client/src/components/search/User.jsx
+++ client/src/components/search/User.jsx
@@ -1,23 +1,19 @@
-import ServiceImg from './UserImg'
+import UserImg from './UserImg'
 import { Link } from 'react-router-dom'
 
 const User = ({ user }) => {
+  console.log('user', user)
+
   return (
     <div className="self-stretch justify-center flex flex-col max-w-[450px] shadow-lg">
-      <ServiceImg
-        serviceName={user.service?.serviceName}
-        serviceLogo={user.service?.serviceLogo}
+      <UserImg
+        fullName={user.fullName}
+        alumniProfilePicture={user?.alumniProfilePicture}
       />
       <div className="p-2 px-4 flex flex-col flex-grow">
-        <h2 className="font-semibold text-xl mt-2">
-          {user.service?.serviceName}
-        </h2>
-        <div className="text-base mb-4">{user.fullName}</div>
-        <p className="mb-6 overflow-hidden text-ellipsis line-clamp-4">
-          Lorem ipsum this will hopefully be a field that described the service
-          that the user offers. Will probably need this for field for their
-          public profile page anyway?
-        </p>
+        <h2 className="font-semibold text-xl mt-2">{user.fullName}</h2>
+        <h2>{user.suburb}</h2>
+
         <div className="mt-auto flex gap-4 justify-between w-[100%] md:w-[50%]">
           <Link
             className="py-2 px-4 bg-primary text-white rounded flex-grow text-center border-primary border-2 hover:bg-primary/70 hover:border-transparent]"
