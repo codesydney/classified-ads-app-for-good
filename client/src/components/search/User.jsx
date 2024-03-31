dc2f22a36c360c80fedeb4443b4323bc2dfbf69f
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-31 16:30:46 +1100

    CA-92 include link and services

--- client/src/components/search/User.jsx
+++ client/src/components/search/User.jsx
@@ -2,8 +2,6 @@ import UserImg from './UserImg'
 import { Link } from 'react-router-dom'
 
 const User = ({ user }) => {
-  console.log('user', user)
-
   return (
     <div className="self-stretch justify-center flex flex-col max-w-[450px] shadow-lg">
       <UserImg
@@ -12,7 +10,37 @@ const User = ({ user }) => {
       />
       <div className="p-2 px-4 flex flex-col flex-grow">
         <h2 className="font-semibold text-xl mt-2">{user.fullName}</h2>
-        <h2>{user.suburb}</h2>
+        {user?.suburb && <h2>{user.suburb}</h2>}
+
+        {user?.education && (
+          <div className="mt-[10px]">
+            <h2>{user.education.college}</h2>
+            <h2>{user.education.course}</h2>
+            <h2>{user.education.yearGraduated}</h2>
+          </div>
+        )}
+
+        {user.service && (
+          <div className="mt-[10px] w-[100%] flex flex-wrap mb-[10px]">
+            <div
+              style={{
+                overflowWrap: 'break-word',
+                wordWrap: 'break-word',
+                hyphens: 'auto',
+                whiteSpace: 'pre-wrap',
+                wordBreak: 'break-all',
+              }}
+            >
+              <a
+                href={`${user.service.serviceUrl}`}
+                target="_blank"
+                className="hover:underline hover:text-blue-500 flex-wrap text-[12px]"
+              >
+                {user.service.serviceName}
+              </a>
+            </div>
+          </div>
+        )}
 
         <div className="mt-auto flex gap-4 justify-between w-[100%] md:w-[50%]">
           <Link
