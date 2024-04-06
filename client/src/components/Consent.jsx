4a748fa185ccfb9c85759812bc14a563a92630bf
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-06 14:24:16 +1100

    CA-111 applied consent to Sign Up and Login pages

--- client/src/components/Consent.jsx
+++ client/src/components/Consent.jsx
@@ -1,6 +1,6 @@
 const Consent = () => {
   return (
-    <p>
+    <p className="text-[11px] text-justify">
       Signing up and using the service means agreeing with the below consent.
       Consent clause 'By providing your personal information, you consent to its
       use for any purpose of the USTAA including publishing photos and
