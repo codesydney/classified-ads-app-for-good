e465dd3a25a3c9e7427a6096aa8d7494cba16d70
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-06 14:19:12 +1100

    CA-111 constructed the consent component

---
+++ client/src/components/Consent.jsx
@@ -0,0 +1,12 @@
+const Consent = () => {
+  return (
+    <p>
+      Signing up and using the service means agreeing with the below consent.
+      Consent clause 'By providing your personal information, you consent to its
+      use for any purpose of the USTAA including publishing photos and
+      non-personally identifiable information on USTAA's social media pages.
+    </p>
+  )
+}
+
+export default Consent
