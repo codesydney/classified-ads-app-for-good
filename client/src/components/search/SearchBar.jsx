4529977f6d895143c890ac984f3b6464d4dde0b9
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-08 06:07:38 +1000

    CA-improvements updated placeholder label

--- client/src/components/search/SearchBar.jsx
+++ client/src/components/search/SearchBar.jsx
@@ -19,9 +19,9 @@ const SearchBar = () => {
   return (
     <input
       id="search-bar"
-      aria-label="Search"
+      aria-label="Search Alumni"
       type="search"
-      placeholder="Search"
+      placeholder="Search Alumni"
       className="w-full px-4 py-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent my-8"
       value={value}
       onChange={handleChange}
