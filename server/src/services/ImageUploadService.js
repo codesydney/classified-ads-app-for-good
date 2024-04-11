b0133b8f5daddab85801b7972e6bdfb3baa99169
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-11 13:09:54 +1000

    more fkn logs

--- server/src/services/ImageUploadService.js
+++ server/src/services/ImageUploadService.js
@@ -19,6 +19,9 @@ const uploadImageToS3 = async (file, bucketName) => {
   }
 
   console.log('uploadParams', uploadParams)
+  console.log('process.env.AWS_REGION', process.env.AWS_REGION)
+  console.log('process.env.AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID)
+  console.log('process.env.AWS_SECRET', process.env.AWS_SECRET_ACCESS_KEY)
 
   try {
     const parallelUploads3 = new Upload({
