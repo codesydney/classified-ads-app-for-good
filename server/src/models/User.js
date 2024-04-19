6e7395095b696f9825d53d28e2e76091b93712c9
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-19 17:57:39 +1000

    CA-144 added additional flag for isAdmin

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -144,6 +144,10 @@ const UserSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
