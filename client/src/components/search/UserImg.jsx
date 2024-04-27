fd4970740db92b4a0c81dbef6afcb2fa761c8e3c
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-28 08:06:26 +1000

    CA-147 default profile upon sign up

--- client/src/components/search/UserImg.jsx
+++ client/src/components/search/UserImg.jsx
@@ -1,10 +1,11 @@
-import defaultUserLogo from '../../assets/serviceImgPlaceholder.svg'
-
 const UserImg = ({ fullName, alumniProfilePicture }) => {
   return (
     <div>
       <img
-        src={alumniProfilePicture || defaultUserLogo}
+        src={
+          alumniProfilePicture ||
+          'https://ustaa.s3.ap-southeast-2.amazonaws.com/images/USTAA-Profile.png'
+        }
         alt={`${fullName} profile picture`}
         className="w-full aspect-ratio-4/3 object-contain rounded-t-md"
       />
