c10c104cfd35aebc492e127614766f0a4e75a636
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2024-05-26 10:09:41 +1000

    Add styling to admin page title

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -4,7 +4,9 @@ import AdminSearchResults from '../components/admin/searchResults/AdminSearchRes
 const AdminPanel = () => {
   return (
     <div>
-      <h1>Admin Panel</h1>
+      <h1 className="p-4 pb-0 m-auto max-w-[1444px] font-bold md:text-xl lg:text-2xl">
+        Admin Panel
+      </h1>
       <AdminSearchBar />
       <AdminSearchResults />
     </div>
